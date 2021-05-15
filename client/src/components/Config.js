57e3f0e078943f14c338ad5010f6385d43179449
daelee <apm5018@naver.com>
2021-05-15 21:18:35 +0900

    API_URL setting

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+//TheMoviedb API
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
