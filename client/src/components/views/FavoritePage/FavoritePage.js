3c9d74c1bc94dac79efe27ce89e45525df4c9a7e
daelee <apm5018@naver.com>
2021-05-23 21:15:57 +0900

    Favorited page - Remove function, image popover

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import './favorite.css';
 import Axios from 'axios';
+import {Popover} from 'antd';
+import {IMAGE_BASE_URL} from '../../Config';
 
 function FavoritePage() {
 
     const [Favorites, setFavorites] = useState([])
-
-    useEffect(() => {
+    const fatchFavoriteList = () => {
         Axios.post('/api/favorite/getFavoritedMovie', {userFrom: localStorage.getItem('userId')})
         .then(response => {
             if(response.data.success) {
@@ -15,7 +16,48 @@ function FavoritePage() {
                 alert("Failed to get Favorited movies")
             }
         })
+    }
+
+    useEffect(() => {
+        fatchFavoriteList()
     }, [])
+
+    const onClickDelete = (movieId, userFrom) => {
+
+        const variable = {
+            movieId,
+            userFrom
+        }
+
+        Axios.post('/api/favorite/removeFavorite', variable)
+        .then(response => {
+            if(response.data.success) {
+                fatchFavoriteList()
+            } else {
+                alert ("Failed to remove favorite.")
+            }
+        })
+    }
+
+    const renderTable = Favorites.map((favorites, index) => {
+
+        const content = (
+            <div>
+                {favorites.moviePost ?
+                    <img src={`${IMAGE_BASE_URL}w500${favorites.moviePost}`} /> : "no image"
+                }
+            </div>
+        )      
+        
+        //Popover는 이미지tooltip을 위함
+        return <tr key ={index}>
+            <Popover content = {content} title={`${favorites.movieTitle}`}>
+                <td>{favorites.movieTitle}</td>
+            </Popover>                            
+            <td>{favorites.movieRunTime}</td>
+            <td><button onClick={() => onClickDelete(favorites.movieId, favorites.userFrom)}>Remove</button></td>
+        </tr>
+    })
 
     return (
         <div style = {{width: '85%', margin: '3rem auto'}}>
@@ -31,13 +73,7 @@ function FavoritePage() {
                     </tr>
                 </thead>
                 <tbody>
-                    {Favorites.map((favorites, index) => (
-                        <tr key ={index}>
-                            <td>{favorites.movieTitle}</td>
-                            <td>{favorites.movieRunTime}</td>
-                            <td><button>Remove</button></td>
-                        </tr>
-                    ))}
+                    {renderTable}
                 </tbody>
             </table>
             
